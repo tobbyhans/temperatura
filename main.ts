840a93dae0f5794eded9f8c3b6cc149ea183920e
tobbyhans <everyfix@hotmail.com>
2023-12-07 17:54:54 +0100

    numeros

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    numero += 1
+    basic.showNumber(numero)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
+    if (numero >= 9) {
+        numero = 0
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    numero += -1
+    basic.showNumber(numero)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+    if (numero <= 9) {
+        numero = 1
+    }
+})
+let numero = 0
+basic.showNumber(0)
+numero = 0
